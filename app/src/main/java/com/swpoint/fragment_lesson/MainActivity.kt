dcb9d4706dcbe2afe84ed3873a00b664043f2b8d
eagledlc <eagle-dlc@yandex.ru>
2021-03-15 00:17:42 +0300

    123

--- app/src/main/java/com/swpoint/fragment_lesson/MainActivity.kt
+++ app/src/main/java/com/swpoint/fragment_lesson/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.swpoint.fragment_lesson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.swpoint.fragment_lesson.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val intent = Intent(this, SecondActivity::class.java)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/com/swpoint/fragment_lesson/ui/main/MainFragment.kt
+++ app/src/main/java/com/swpoint/fragment_lesson/ui/main/MainFragment.kt
@@ -19,6 +19,7 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
