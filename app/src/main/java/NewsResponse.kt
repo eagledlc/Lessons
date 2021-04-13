ead03e0aaee8213fd96cd1df297c5a100dd20dc8
Eagle <eagle-dlc@yandex.ru>
2021-04-13 20:30:38 +0300

    same

--- app/src/main/java/NewsResponse.kt
+++ app/src/main/java/NewsResponse.kt
@@ -1,4 +1,4 @@
-data class test(
+data class NewsResponse(
     val articles: List<Article>,
     val status: String,
     val totalResults: Int
---
+++ app/src/main/java/util/Constants.kt
@@ -0,0 +1,8 @@
+package util
+
+class Constants {
+    companion object{
+        const val API_KEY = "52eeb5057508404bbf7e74fba5e2a083"
+            const val BASE_URL = "https://newsapi.org"
+    }
+}
